171515dae1786d38d6021bf08ce23eb283345ad9
Victor Davydenko <acc.davydenko@gmail.com>
2023-07-31 23:06:55 +0300

    add auth

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Login = () => {
+	const handleSignIn = (response) => {
+		console.log(response);
+	};
+	useEffect(() => {
+		/* global google */
+		google.accounts.id.initialize({
+			client_id: '24231810214-h3h28cnefvc9aqfnthlb6643f8ddqq9v.apps.googleusercontent.com',
+			callback: handleSignIn,
+		});
+		google.accounts.id.renderButton(document
+			.getElementById('login'), { theme: 'outline', size: 'large' });
+	}, []);
 	return (
 		<div>
-            LoginPage
+           	<div id='login'></div>
 		</div>
 	);
 };
